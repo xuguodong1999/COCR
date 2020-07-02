7c9bbe719ebd2d5f2a73f77c571ea1f011a10882
Xuguodong1999 <171860633@smail.nju.edu.cn>
2020-07-02 23:47:01 +0800

    完成g++/msvc上3rdparty的自动构建和样例运行

--- src/main.cpp
+++ src/main.cpp
@@ -1,252 +1,124 @@
-//#include <QApplication>
-//#include <QWidget>
-//int main(int argc,char**argv){
-//    QApplication a(argc,argv);
-//    (new QWidget)->show();
-//    return a.exec();
-//}
-#include <opencv2/opencv.hpp>
+#define TEST_ALL_LIB_WORK
+#ifdef TEST_ALL_LIB_WORK
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
 #include <iostream>
 
+void helloRapidJson() {
+    using namespace rapidjson;
+    using namespace std;
+    // 1. 把 JSON 解析至 DOM。
+    const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
+    // 2. 利用 DOM 作出修改。
+    Value &s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+    // 3. 把 DOM 转换（stringify）成 JSON。
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    // Output {"project":"rapidjson","stars":11}
+    cout << buffer.GetString() << endl;
+}
 
-#include <openbabel/atom.h>
+#include <QWidget>
 
-#include <string>
+void helloQt() {
+    (new QWidget)->show();
+}
+
 #include <openbabel/mol.h>
-#include <openbabel/obiter.h>
 #include <openbabel/builder.h>
 #include <openbabel/forcefield.h>
 #include <openbabel/obconversion.h>
+#include <iostream>
 
-using namespace OpenBabel;
-using namespace std;
-using namespace cv;
-
-class ForceField {
-public:
-    ForceField();
-
-    ~ForceField();
-
-    std::string getCoordinate(
-            const char *inData,
-            const char *inType = "smiles",
-            const char *outType = "mol2",
-            const char *forcefield = "uff"
-    );
-
-    double getEnergy(
-            const char *inData,
-            const char *inType = "mol2",
-            const char *forcefield = "uff"
-    );
-
-    double getEnergy();
-
-private:
-    double energy;
-    static bool pluginLoaded;
-};
-
-bool ForceField::pluginLoaded = false;
-
-ForceField::ForceField() {
-    if (pluginLoaded) {
-        return;
-    }
-    OBPlugin::LoadAllPlugins();
-    pluginLoaded = true;
-}
-
-ForceField::~ForceField() {
-
-}
-
-string ForceField::getCoordinate(
-        const char *inData,
-        const char *inType,
-        const char *outType,
-        const char *forcefield
-) {
+void helloOpenBabel() {
+    using namespace OpenBabel;
+    using namespace std;
     OBConversion conv;
-    OBFormat *format_in = conv.FindFormat(inType);
-    OBFormat *format_out = conv.FindFormat(outType);
-    if (!format_in || !format_out || !conv.SetInAndOutFormats(format_in, format_out)) {
-        string tmpInfo = "inType = ";
-        tmpInfo += inType;
-        tmpInfo += ", outType = ";
-        tmpInfo += outType;
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                tmpInfo.c_str()
-//        );
-        return "";
-    }
-    stringstream ism(inData);
-    if (ism.eof()) {
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                "empty input data!"
-//        );
-        return "";
-    }
+    auto format_in = conv.FindFormat("smiles");
+    auto format_out = conv.FindFormat("pdb");
+    stringstream ism("c1ccccc1");
     OBMol mol;
-    if (!conv.Read(&mol, &ism)) {
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                "fail to perform conv.Read(&mol, &ism)"
-//        );
-        return "";
-    }
-    if (mol.Empty()) {
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                "fail to check mol.Empty()"
-//        );
-        return "";
-    }
-    OBForceField *pFF = OBForceField::FindForceField(forcefield);
-    if (!pFF) {
-        string tmpInfo = "could not find forcefield: ";
-        tmpInfo += forcefield;
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                tmpInfo.c_str()
-//        );
-        return "";
-    }
-
-
+    conv.SetInFormat(format_in);
+    conv.SetOutFormat(format_out);
+    conv.Read(&mol, &ism);
+    OBForceField *pFF = OBForceField::FindForceField("uff");
     OBBuilder builder;
-    //builder.CorrectStereoAtoms(mol);
-    //builder.CorrectStereoBonds(mol);
     builder.Build(mol);
     mol.AddHydrogens(false, true);
-    //FOR_ATOMS_OF_MOL(atom, mol) {
-    //	cout << "id: " << atom->GetId()
-    //		<< " x: " << atom->x()<< " y: " << atom->y()<< " z: " << atom->z() << endl;
-    //}
-
-    if (!pFF->Setup(mol)) {
-//        throwErrorBox(
-//                "Error in ForceField::getCoordinate",
-//                "could not setup force field"
-//        );
-        return "";
-    }
-
-    //pFF->SteepestDescent(500, 1.0e-4);
-    //pFF->WeightedRotorSearch(250, 50);
-    //pFF->SteepestDescent(500, 1.0e-6);
-    pFF->SteepestDescent(100, 1.0e-4);
-    pFF->WeightedRotorSearch(50, 10);
-    pFF->SteepestDescent(100, 1.0e-6);
+    pFF->Setup(mol);
     pFF->UpdateCoordinates(mol);
-    energy = pFF->Energy();
-
     stringstream osm;
     conv.Write(&mol, &osm);
-    return osm.str();
+    cout << osm.str() << endl;
 }
 
-double ForceField::getEnergy() {
-    return energy;
+#include <Eigen/Eigen>
+#include <iostream>
+
+void helloEigen() {
+    using namespace Eigen;
+    using namespace std;
+    Vector2d p1(1, 2);
+    cout << p1 + p1 * 1.2 << endl;
 }
 
-double ForceField::getEnergy(
-        const char *inData,
-        const char *inType,
-        const char *forcefield
-) {
-    OBConversion conv;
-    OBFormat *format_in = conv.FindFormat(inType);
+#include <opencv2/dnn.hpp>
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <vector>
 
-    if (!format_in || !conv.SetInFormat(format_in)) {
-        string tmpInfo = "inType = ";
-        tmpInfo += inType;
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                tmpInfo.c_str()
-//        );
-        return -1;
+void helloOpenCV() {
+    using namespace cv;
+    using namespace dnn;
+    using namespace std;
+    using namespace chrono;
+    const char *cfg = "../weights/enetb0.cfg";
+    const char *weights = "../weights/enetb0.weights";
+    auto net = dnn::readNetFromDarknet(cfg, weights);
+    net.setPreferableBackend(dnn::DNN_BACKEND_OPENCV);
+    net.setPreferableTarget(dnn::DNN_TARGET_CPU);
+    auto outLayerIndices = net.getUnconnectedOutLayers();
+    auto layerNames = net.getLayerNames();
+    vector<String> outBlobNames(outLayerIndices.size());
+    for (auto i = 0; i < outLayerIndices.size(); i++) {
+        outBlobNames.at(i) = layerNames.at(outLayerIndices.at(i) - 1);
     }
-    stringstream ism(inData);
-    if (ism.eof()) {
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                "empty input data!"
-//        );
-        return -1;
-    }
-    OBMol mol;
-    if (!conv.Read(&mol, &ism)) {
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                "fail to perform conv.Read(&mol, &ism)"
-//        );
-        return -1;
-    }
-    if (mol.Empty()) {
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                "fail to check mol.Empty()"
-//        );
-        return -1;
-    }
-
-    OBForceField *pFF = OBForceField::FindForceField(forcefield);
-    if (!pFF) {
-        string tmpInfo = "could not find forcefield: ";
-        tmpInfo += forcefield;
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                tmpInfo.c_str()
-//        );
-        return -1;
-    }
-
-    OBBuilder builder;
-    builder.Build(mol);
-
-    mol.AddHydrogens(false, true);
-    if (!pFF->Setup(mol)) {
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                "could not setup force field"
-//        );
-        return -1;
-    }
-    energy = pFF->Energy(false);
-    if (!isfinite(energy)) {
-        string tmpInfo = "Error calculating energy:infinite result for ";
-        tmpInfo += mol.GetTitle();
-//        throwErrorBox(
-//                "Error in ForceField::getEnergy",
-//                tmpInfo.c_str()
-//        );
-        return -1;
-    }
-    return energy;
+    auto img = imread("../ui/1a0n.jpg", IMREAD_COLOR);
+    Size inputSize(img.rows - img.rows % 32, img.cols - img.cols % 32);
+    Mat vec4d;
+    blobFromImage(img, vec4d, 1 / 255.0, inputSize);
+    net.setInput(vec4d);
+    auto start = system_clock::now();
+    vector<Mat> outs;
+    net.forward(outs, outBlobNames);
+    auto end = system_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "net.forward costs "
+         << duration.count() * milliseconds::period::num / milliseconds::period::den
+         << " ms" << endl;
 }
 
 #include <QApplication>
-#include <QWidget>
+#include <exception>
 
 int main(int argc, char **argv) {
-    QApplication app(argc, argv);
-    QWidget w;
-    w.show();
-    ForceField f;
-    auto coor = f.getCoordinate("c1ccccc1", "smiles", "mol2", "mmff94");
-    cout << "coor=" << coor << endl;
-//    Mat img = imread("/home/xgd/Pictures/backup/4.jpg");
-    Mat img=imread("C:\\Users\\xgd\\Pictures\\2017\\IMG_20170630_125619.jpg");
-    if (img.empty()) {
-        cout << "打开图像失败！" << endl;
-    } else {
-        imshow("image", img);
-        waitKey();
+    QApplication a(argc, argv);
+    try {
+        helloEigen();
+        helloOpenBabel();
+        helloOpenCV();
+        helloQt();
+        helloRapidJson();
+    } catch (std::exception e) {
+        std::cout << e.what() << std::endl;
     }
-    return app.exec();
+    return a.exec();
 }
 
+#endif //!TEST_1
