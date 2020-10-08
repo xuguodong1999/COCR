8357647b86c73c18d92ee278499ecec345768c4d
Xuguodong1999 <bugxu@outlook.com>
2020-10-08 23:27:13 +0800

    cermvboprtbhmr

--- cpp/demo/main.cpp
+++ cpp/demo/main.cpp
@@ -1,163 +1,37 @@
-#include "neural_net.h"
-
-#include <torch/torch.h>
-
-#include <opencv2/opencv.hpp>
-#include <boost/graph/adjacency_list.hpp>
 #include <QFont>
 #include <QFontDatabase>
 #include <QDebug>
 #include <QTranslator>
 #include <QApplication>
+#include <QQmlApplicationEngine>
 #include <QWidget>
-
-#include <iostream>
-#include <iomanip>
-
-void demo_torch() {
-    std::cout << "FeedForward Neural Network\n\n";
-
-    // Device
-    auto cuda_available = torch::cuda::is_available();
-    torch::Device device(cuda_available ? torch::kCUDA : torch::kCPU);
-    std::cout << (cuda_available ? "CUDA available. Training on GPU." : "Training on CPU.") << '\n';
-
-    // Hyper parameters
-    const int64_t input_size = 784;
-    const int64_t hidden_size = 500;
-    const int64_t num_classes = 10;
-    const int64_t batch_size = 100;
-    const size_t num_epochs = 50;
-    const double learning_rate = 0.001;
-
-    const std::string MNIST_data_path = "C:/Users/xgd/.keras/datasets/pytorch-cpp/mnist/";
-
-    // MNIST Dataset
-    auto train_dataset = torch::data::datasets::MNIST(MNIST_data_path)
-            .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-            .map(torch::data::transforms::Stack<>());
-
-    // Number of samples in the training set
-    auto num_train_samples = train_dataset.size().value();
-
-    auto test_dataset = torch::data::datasets::MNIST(MNIST_data_path, torch::data::datasets::MNIST::Mode::kTest)
-            .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
-            .map(torch::data::transforms::Stack<>());
-
-    // Number of samples in the testset
-    auto num_test_samples = test_dataset.size().value();
-
-    // Data loaders
-    auto train_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(
-            std::move(train_dataset), batch_size);
-
-    auto test_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
-            std::move(test_dataset), batch_size);
-
-    // Neural Network model
-    NeuralNet model(input_size, hidden_size, num_classes);
-    model->to(device);
-
-    // Optimizer
-    torch::optim::SGD optimizer(model->parameters(), torch::optim::SGDOptions(learning_rate));
-
-    // Set floating point output precision
-    std::cout << std::fixed << std::setprecision(4);
-
-    std::cout << "Training...\n";
-
-    // Train the model
-    for (size_t epoch = 0; epoch != num_epochs; ++epoch) {
-        // Initialize running metrics
-        double running_loss = 0.0;
-        size_t num_correct = 0;
-
-        for (auto& batch : *train_loader) {
-            auto data = batch.data.view({batch_size, -1}).to(device);
-            auto target = batch.target.to(device);
-
-            // Forward pass
-            auto output = model->forward(data);
-            auto loss = torch::nn::functional::cross_entropy(output, target);
-
-            // Update running loss
-            running_loss += loss.item<double>() * data.size(0);
-
-            // Calculate prediction
-            auto prediction = output.argmax(1);
-
-            // Update number of correctly classified samples
-            num_correct += prediction.eq(target).sum().item<int64_t>();
-
-            // Backward and optimize
-            optimizer.zero_grad();
-            loss.backward();
-            optimizer.step();
-        }
-
-        auto sample_mean_loss = running_loss / num_train_samples;
-        auto accuracy = static_cast<double>(num_correct) / num_train_samples;
-
-        std::cout << "Epoch [" << (epoch + 1) << "/" << num_epochs << "], Trainset - Loss: "
-                  << sample_mean_loss << ", Accuracy: " << accuracy << '\n';
-    }
-
-    std::cout << "Training finished!\n\n";
-    std::cout << "Testing...\n";
-
-    // Test the model
-    model->eval();
-    torch::NoGradGuard no_grad;
-
-    double running_loss = 0.0;
-    size_t num_correct = 0;
-
-    for (const auto& batch : *test_loader) {
-        auto data = batch.data.view({batch_size, -1}).to(device);
-        auto target = batch.target.to(device);
-
-        auto output = model->forward(data);
-
-        auto loss = torch::nn::functional::cross_entropy(output, target);
-
-        running_loss += loss.item<double>() * data.size(0);
-
-        auto prediction = output.argmax(1);
-
-        num_correct += prediction.eq(target).sum().item<int64_t>();
-    }
-
-    std::cout << "Testing finished!\n";
-
-    auto test_accuracy = static_cast<double>(num_correct) / num_test_samples;
-    auto test_sample_mean_loss = running_loss / num_test_samples;
-
-    std::cout << "Testset - Loss: " << test_sample_mean_loss << ", Accuracy: " << test_accuracy << '\n';
-}
-
-void demo_opencv() {
-    auto img = cv::imread("C:/Users/xgd/Pictures/workspace/xiaoqingwa.png");
-    cv::imshow("demo", img);
-    cv::waitKey(0);
-}
-
+#include <QDir>
 int main(int argc, char **argv) {
 #ifdef WIN32
     _putenv("BABEL_DATADIR=C:\\StaticLibs\\openbabel-3.1.1\\data");
 #elif defined(unix)
     putenv("BABEL_DATADIR=/home/xgd/install/openbabel/share/openbabel/3.1.0");
 #endif
-//    demo_opencv();
-    demo_torch();
-//    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-//    QApplication a(argc, argv);
-//    QFontDatabase::addApplicationFont(QStringLiteral(":/fonts/cocr.simhei.ttf"));
-//    QGuiApplication::setFont(QFont("simhei"));
-//    QTranslator translator;
-//    if (translator.load(QStringLiteral(":/demo_zh_CN.qm"))) {
-//        qInfo() << "load translation file: "
-//                << QApplication::installTranslator(&translator);
-//    }
-//    (new QWidget)->showMaximized();
-//    return a.exec();
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont(QStringLiteral(":/fonts/cocr.simhei.ttf"));
+    QGuiApplication::setFont(QFont("simhei"));
+    QDir dir(":/");
+    qInfo()<<dir.entryList();
+    static QString fuck = QWidget::tr("test");
+    QTranslator translator;
+    if (translator.load(QStringLiteral(":/demo_zh_CN.qm"))) {
+        qInfo() << "load translation file: "
+                << QApplication::installTranslator(&translator);
+    }
+//    (new QWidget)->showNormal();
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &a, [url](QObject *obj, const QUrl &objUrl) {
+                if (!obj && url == objUrl)
+                    QCoreApplication::exit(-1);
+            }, Qt::QueuedConnection);
+    engine.load(url);
+    return a.exec();
 }
---
+++ cpp/demo/res/demo_zh_CN.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Page1Form.ui</name>
+    <message>
+        <location filename="../Page1Form.ui.qml" line="9"/>
+        <source>Page 1</source>
+        <translation>第一页</translation>
+    </message>
+    <message>
+        <location filename="../Page1Form.ui.qml" line="15"/>
+        <source>You are on Page 1.</source>
+        <translation>你在第一页哦</translation>
+    </message>
+</context>
+<context>
+    <name>Page2Form.ui</name>
+    <message>
+        <location filename="../Page2Form.ui.qml" line="9"/>
+        <source>Page 2</source>
+        <translation>第二页</translation>
+    </message>
+    <message>
+        <location filename="../Page2Form.ui.qml" line="15"/>
+        <source>You are on Page 2.</source>
+        <translation>你在第二页哈</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="10"/>
+        <source>Tabs</source>
+        <translation>标题</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="34"/>
+        <source>Page 1</source>
+        <translation>第一页</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="37"/>
+        <source>Page 2</source>
+        <translation>第二页</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="40"/>
+        <source>Page 3</source>
+        <translation>第三页</translation>
+    </message>
+    <message>
+        <source>nice to meet you</source>
+        <translation type="vanished">见到你真高兴</translation>
+    </message>
+</context>
+</TS>
