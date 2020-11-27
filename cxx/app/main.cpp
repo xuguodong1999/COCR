996f14f7a7af14eee03558b2e5d96de7fbe68baa
Xuguodong1999 <bugxu@outlook.com>
2020-11-28 00:42:39 +0800

    xdoienfvrie

--- cxx/app/main.cpp
+++ cxx/app/main.cpp
@@ -27,21 +27,24 @@ inline void addTranslator() {
     }
 }
 
-#include <Qt3DCore/QEntity>
 #include <Qt3DRender/QCamera>
 #include <Qt3DRender/QCameraLens>
+
+#include <Qt3DCore/QEntity>
 #include <Qt3DCore/QTransform>
 #include <Qt3DCore/QAspectEngine>
 
 #include <Qt3DInput/QInputAspect>
 
-#include <Qt3DRender/QRenderAspect>
+#include <Qt3DExtras/QOrbitCameraController>
 #include <Qt3DExtras/QForwardRenderer>
+#include <Qt3DRender/QRenderAspect>
 #include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DExtras/QDiffuseSpecularMaterial>
 #include <Qt3DExtras/QCylinderMesh>
 #include <Qt3DExtras/QSphereMesh>
 #include <Qt3DExtras/QTorusMesh>
-
+#include <Qt3DExtras/Qt3DWindow>
 
 int main(int argc, char **argv) {
     qputenv("QML_DISABLE_DISK_CACHE", "1");
@@ -52,9 +55,51 @@ int main(int argc, char **argv) {
     addFontData();
     addTranslator();
 
-    qmlRegisterType<SketchItem>(SketchItem::uri, 1, 0,
-                                SketchItem::qmlName);
-    QQmlApplicationEngine engine(QUrl("qrc:/main.qml"));
+//    qmlRegisterType<SketchItem>(SketchItem::uri, 1, 0,
+//                                SketchItem::qmlName);
+//    QQmlApplicationEngine engine(QUrl("qrc:/main.qml"));
+
+    auto *scene = new Qt3DCore::QEntity;
+
+    auto *sphereEntity = new Qt3DCore::QEntity(scene);
+    auto *sphereMesh = new Qt3DExtras::QSphereMesh;
+    sphereMesh->setRadius(3);
+    sphereMesh->setGenerateTangents(true);
+    sphereMesh->setSlices(128);
+
+    auto *material = new Qt3DExtras::QPhongMaterial(scene);
+//    material->setSpecular(Qt::darkRed);
+
+//    auto *material = new Qt3DExtras::QDiffuseSpecularMaterial(scene);
+
+    material->setAmbient(Qt::lightGray);
+    material->setDiffuse(Qt::darkGray);
+    material->setShininess(100);
+
+
+    Qt3DCore::QTransform *sphereTransform = new Qt3DCore::QTransform;
+    sphereTransform->setScale(0.3);
+    sphereTransform->setTranslation(QVector3D(-3,-3,0));
+
+    sphereEntity->addComponent(material);
+    sphereEntity->addComponent(sphereMesh);
+    sphereEntity->addComponent(sphereTransform);
+
+    Qt3DExtras::Qt3DWindow view;
+    // Camera
+    auto *camera = view.camera();
+    camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.01f, 1024000.0f);
+    camera->setPosition(QVector3D(10, 0, 0));
+    camera->setViewCenter(QVector3D(0,0,0));
+
+    // For camera controls
+    auto camController = new Qt3DExtras::QOrbitCameraController(scene);
+    camController->setLinearSpeed( 50.0f );
+    camController->setLookSpeed( 180.0f );
+    camController->setCamera(camera);
+
+    view.setRootEntity(scene);
+    view.showMaximized();
 
     return app.exec();
 }
