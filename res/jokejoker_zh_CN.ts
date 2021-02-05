3bafb6171dbfcd404c5afd3b1b33d8cfc5556e3d
Xuguodong1999 <bugxu@outlook.com>
2021-02-05 20:25:21 +0800

    first commit: with cpp17 compatible openbabel

---
+++ res/jokejoker_zh_CN.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/main.qml" line="8"/>
+        <source>Hello World!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main2</name>
+    <message>
+        <location filename="../src/main2.qml" line="8"/>
+        <source>Hello World hahaha!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <opencv2/core/mat.hpp>
+int main(int argc, char *argv[])
+{
+    cv::Mat mat;
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
