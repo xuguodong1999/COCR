5e576a585eeb8c8962ede083cbc3bef5088ce648
Xuguodong1999 <bugxu@outlook.com>
2021-02-05 23:06:11 +0800

    build: set OpenCV_DIR for MSVC

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <opencv2/core/mat.hpp>
-int main(int argc, char *argv[])
-{
-    cv::Mat mat;
+int main(int argc, char *argv[]) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl) {
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
