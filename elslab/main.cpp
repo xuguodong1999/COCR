edbb4407539c7769e2053b22b127fd949429f0f3
xuguodong1999 <171860633@smail.nju.edu.cn>
2022-02-27 16:17:04 +0800

    test: add a queue-based data loader
    Signed-off-by: xuguodong1999 <171860633@smail.nju.edu.cn>

--- elslab/main.cpp
+++ elslab/main.cpp
@@ -10,29 +10,40 @@
 #include <torch/data/transforms/stack.h>
 #include <torch/data/datasets/map.h>
 #include <torch/nn/functional/loss.h>
+#include <concurrentqueue/blockingconcurrentqueue.h>
+
+const torch::DeviceType runtime_device = torch::kCUDA;
+const size_t batch_size = 64;
+const size_t LAYER_BASE_WIDTH = 128;
+const size_t cached_batch = 24;
+double learning_rate = 0.001;
+const size_t num_epochs = 20;
+const size_t worker_num = 16;
+const size_t DATA_SET_SIZE = 1024;
+const size_t width = 320, height = 320, out_channel = 10, in_channel = 1;
+size_t epoch = 0;
 
 using data_item_type = std::pair<torch::Tensor, torch::Tensor>;
 
+inline data_item_type get_data_with_samples(const int64_t &sample_num, const torch::DeviceType device = torch::kCPU) {
+    auto input = torch::ones({sample_num, in_channel, width, height})
+            .toType(torch::kFloat32).to(device, true);
+    auto label = torch::ones({sample_num, out_channel})
+            .toType(torch::kFloat32).to(device, true);
+    return {input, label};
+}
+
 class ElsDataset : public torch::data::datasets::Dataset<ElsDataset> {
-    inline static const size_t DATA_SET_SIZE = 10240;
     torch::Tensor input, label;
 public:
     ElsDataset() {
-        input = torch::ones(
-                {1, 416, 640}, torch::TensorOptions().requires_grad(false))
-                        .toType(torch::kFloat32) * 0.2022;
-        label = torch::ones(
-                {10}, torch::TensorOptions().requires_grad(false))
-                        .toType(torch::kFloat32) * 0.1;
+        auto[a, b] = get_data_with_samples(1);
+        input = a.squeeze(0);
+        label = b.squeeze(0);
     }
 
     torch::data::Example<> get(size_t index) override {
-//        auto input = torch::ones(
-//                {1, 416, 640}, torch::TensorOptions().requires_grad(false))
-//                             .toType(torch::kFloat32) * 0.2022;
-//        auto label = torch::ones(
-//                {10}, torch::TensorOptions().requires_grad(false))
-//                             .toType(torch::kFloat32) * 0.1;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
         return {input, label};
     }
 
@@ -49,16 +60,16 @@ inline torch::nn::Conv2dOptions conv_opt(
 }
 
 class ElsNetImpl : public torch::nn::Module {
-    inline static const int64_t BASE_SIZE = 16;
+    inline static const int64_t BASE_SIZE = LAYER_BASE_WIDTH;
     torch::nn::Conv2d conv1, conv2;
     torch::nn::Conv2d fc1, fc2, fc3;
 
 public:
-    ElsNetImpl() : conv1(conv_opt(1, BASE_SIZE, 3, 2, 1)),
+    ElsNetImpl() : conv1(conv_opt(in_channel, BASE_SIZE, 3, 2, 1)),
                    conv2(conv_opt(BASE_SIZE, BASE_SIZE * 2, 3, 2, 1)),
                    fc1(conv_opt(BASE_SIZE * 2, BASE_SIZE * 4, 1)),
                    fc2(conv_opt(BASE_SIZE * 4, BASE_SIZE * 8, 1)),
-                   fc3(conv_opt(BASE_SIZE * 8, 10, 1)) {
+                   fc3(conv_opt(BASE_SIZE * 8, out_channel, 1)) {
         register_module("conv1", conv1);
         register_module("conv2", conv2);
         register_module("fc1", fc1);
@@ -81,61 +92,60 @@ public:
 
 TORCH_MODULE(ElsNet);
 
+// just forward and backward, ideal case
+void test_with_fixed_data();
+
+// load tensor from torch::data::dataset, standard usage
+void test_with_torch_dataset();
+
+// load data from self-defined consumer-producer, preload data into gpu
+void test_with_queue();
+
 int main(int argc, char **argv) {
-    const auto device = (torch::cuda::is_available() ? torch::kCUDA : torch::kCPU);
+//    test_with_fixed_data();
+//    test_with_torch_dataset();
+    test_with_queue();
+    return 0;
+}
 
-    const size_t batch_size = 512;
-    double learning_rate = 0.001;
-    const size_t num_epochs = 200;
-    const size_t worker_num = 16;
-    size_t epoch = 0;
+void test_with_fixed_data() {
+    auto model = ElsNet();
+    auto optimizer = torch::optim::Adam(
+            model->parameters(), torch::optim::AdamOptions(learning_rate));
+    model->to(runtime_device);
 
+    const auto[single_input, single_label]=get_data_with_samples(batch_size);
+    auto input = single_input.to(runtime_device, true);
+    auto label = single_label.to(runtime_device, true);
+    while (true) {
+        auto output = model->forward(input);
+        auto loss = torch::nn::functional::cross_entropy(output, label);
+        optimizer.zero_grad();
+        loss.backward();
+        optimizer.step();
+        if (++epoch > num_epochs * DATA_SET_SIZE / batch_size) break;
+    }
+}
+
+void test_with_torch_dataset() {
     auto train_dataset = ElsDataset().map(torch::data::transforms::Stack<>());
-    auto test_dataset = ElsDataset().map(torch::data::transforms::Stack<>());
 
     auto train_loader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(
             std::move(train_dataset), torch::data::DataLoaderOptions()
                     .batch_size(batch_size).workers(worker_num));
 
-    auto test_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
-            std::move(test_dataset), torch::data::DataLoaderOptions()
-                    .batch_size(batch_size).workers(worker_num));
-
     auto model = ElsNet();
     auto optimizer = torch::optim::Adam(
             model->parameters(), torch::optim::AdamOptions(learning_rate));
-    model->to(device);
-    auto single_input = torch::ones(
-            {batch_size, 1, 416, 640}, torch::TensorOptions().requires_grad(false))
-            .toType(torch::kFloat32).to(device);;
-    auto single_label = torch::ones(
-            {batch_size, 10}, torch::TensorOptions().requires_grad(false))
-            .toType(torch::kFloat32).to(device);
-
-    while (true) {
-        auto input = single_input.to(device);
-        auto output = model->forward(input);
-        auto label = single_label.to(device);
-        auto loss = torch::nn::functional::cross_entropy(output, label);
-        optimizer.zero_grad();
-        loss.backward();
-        optimizer.step();
-        // 0ms-100%
-        // 100ms-97%~100%
-        // 150ms-50%~70%
-        // conclusion: torch::data::datasets have bugs for my online data-generation usage
-        // must write it alone
-//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        if (++epoch > num_epochs) break;
-    }
+    model->to(runtime_device, true);
     while (true) {
         double running_loss = 0.0;
         for (const auto &[data, target]: *train_loader) {
-            auto input = data.to(device);
+            auto input = data.to(runtime_device, true);
+            auto label = target.to(runtime_device, true);
             auto output = model->forward(input);
-            auto label = target.to(device);
             auto loss = torch::nn::functional::cross_entropy(output, label);
-//            running_loss += loss.item<double>() * data.size(0);
+            running_loss += loss.item<double>() * data.size(0);
             optimizer.zero_grad();
             loss.backward();
             optimizer.step();
@@ -145,6 +155,54 @@ int main(int argc, char **argv) {
         optimizer.param_groups().front().options().set_lr(learning_rate);
         if (++epoch > num_epochs) break;
     }
+}
 
-    return 0;
+void test_with_queue() {
+    moodycamel::BlockingConcurrentQueue<data_item_type> q;
+    std::mutex mtx; // 全局互斥锁.
+    std::condition_variable cv; // 全局条件变量.
+    bool ready = false; // 全局标志位.
+    bool quit = false;
+
+    std::thread producer([&]() {
+        std::unique_lock<std::mutex> lck(mtx);
+        while (true) {
+            while (!ready)
+                cv.wait(lck);
+            while (q.size_approx() < cached_batch) {
+                auto[batch_input, batch_label]=get_data_with_samples(
+                        batch_size, runtime_device);
+                q.enqueue({batch_input, batch_label});
+            }
+            ready = false;
+            if (quit) break;
+        }
+    });
+    std::thread consumer([&]() {
+        auto model = ElsNet();
+        auto optimizer = torch::optim::Adam(
+                model->parameters(), torch::optim::AdamOptions(learning_rate));
+        model->to(runtime_device);
+        while (true) {
+            if (q.size_approx() < cached_batch / 3) {
+                std::unique_lock<std::mutex> lck(mtx);
+                ready = true;
+                cv.notify_all();
+            }
+            data_item_type item;
+            q.wait_dequeue(item);
+            auto&[input, label]=item;
+            auto output = model->forward(input);
+            auto loss = torch::nn::functional::cross_entropy(output, label);
+            optimizer.zero_grad();
+            loss.backward();
+            optimizer.step();
+            if (++epoch > num_epochs * DATA_SET_SIZE / batch_size) {
+                quit = true;
+                break;
+            }
+        }
+    });
+    producer.join();
+    consumer.join();
 }
