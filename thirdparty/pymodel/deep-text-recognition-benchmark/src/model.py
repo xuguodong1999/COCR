314051c2a60ac1847a569a4c6a42a76e4072d27e
xuguodong1999 <171860633@smail.nju.edu.cn>
2022-03-30 13:32:02 +0800

    add yolox
    Signed-off-by: xuguodong1999 <171860633@smail.nju.edu.cn>

--- thirdparty/pymodel/deep-text-recognition-benchmark/src/model.py
+++ thirdparty/pymodel/deep-text-recognition-benchmark/src/model.py
@@ -16,10 +16,10 @@ limitations under the License.
 
 import torch.nn as nn
 
-from modules.transformation import TPS_SpatialTransformerNetwork
-from modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
-from modules.sequence_modeling import BidirectionalLSTM
-from modules.prediction import Attention
+from transformation import TPS_SpatialTransformerNetwork
+from feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from sequence_modeling import BidirectionalLSTM
+from prediction import Attention
 
 
 class Model(nn.Module):
