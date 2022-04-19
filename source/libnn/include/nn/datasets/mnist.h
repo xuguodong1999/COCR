585eb9cf60280134eeecb98c0b059d185f6d0dbd
xuguodong1999 <171860633@smail.nju.edu.cn>
2022-04-19 17:18:36 +0800

    move mnist to libnn
    Signed-off-by: xuguodong1999 <171860633@smail.nju.edu.cn>

--- source/libnn/include/nn/datasets/mnist.h
+++ source/libnn/include/nn/datasets/mnist.h
@@ -1,48 +1,46 @@
 #pragma once
-
+/**
+ * https://github.com/pytorch/pytorch/blob/b2e79ed5ecabcf4be299dc2ed085223ab5c22fd7/torch/csrc/api/include/torch/data/datasets/mnist.h
+ */
+#include "els_nn_export.h"
 #include <torch/data/datasets/base.h>
 #include <torch/data/example.h>
 #include <torch/types.h>
 
-#include <torch/csrc/Export.h>
 
 #include <cstddef>
 #include <string>
 
-namespace torch {
-namespace data {
-namespace datasets {
 /// The MNIST dataset.
-class TORCH_API MNIST : public Dataset<MNIST> {
- public:
-  /// The mode in which the dataset is loaded.
-  enum class Mode { kTrain, kTest };
+class ELS_NN_EXPORT MNIST : public torch::data::datasets::Dataset<MNIST> {
+public:
+    /// The mode in which the dataset is loaded.
+    enum class Mode {
+        kTrain, kTest
+    };
 
-  /// Loads the MNIST dataset from the `root` path.
-  ///
-  /// The supplied `root` path should contain the *content* of the unzipped
-  /// MNIST dataset, available from http://yann.lecun.com/exdb/mnist.
-  explicit MNIST(const std::string& root, Mode mode = Mode::kTrain);
+    /// Loads the MNIST dataset from the `root` path.
+    ///
+    /// The supplied `root` path should contain the *content* of the unzipped
+    /// MNIST dataset, available from http://yann.lecun.com/exdb/mnist.
+    explicit MNIST(const std::string &root, Mode mode = Mode::kTrain);
 
-  /// Returns the `Example` at the given `index`.
-  Example<> get(size_t index) override;
+    /// Returns the `Example` at the given `index`.
+    torch::data::Example<> get(size_t index) override;
 
-  /// Returns the size of the dataset.
-  optional<size_t> size() const override;
+    /// Returns the size of the dataset.
+    torch::optional<size_t> size() const override;
 
-  /// Returns true if this is the training subset of MNIST.
-  // NOLINTNEXTLINE(bugprone-exception-escape)
-  bool is_train() const noexcept;
+    /// Returns true if this is the training subset of MNIST.
+    // NOLINTNEXTLINE(bugprone-exception-escape)
+    bool is_train() const noexcept;
 
-  /// Returns all images stacked into a single tensor.
-  const Tensor& images() const;
+    /// Returns all images stacked into a single tensor.
+    const torch::Tensor &images() const;
 
-  /// Returns all targets stacked into a single tensor.
-  const Tensor& targets() const;
+    /// Returns all targets stacked into a single tensor.
+    const torch::Tensor &targets() const;
 
- private:
-  Tensor images_, targets_;
+private:
+    torch::Tensor images_, targets_;
 };
-} // namespace datasets
-} // namespace data
-} // namespace torch
